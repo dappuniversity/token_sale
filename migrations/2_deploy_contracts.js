1c04300a71f8e8ff855d77c1f433e3f61f0903c1
Rodolfo Araujo <rodoufu@gmail.com>
2019-07-29 12:12:56 -0300

    Dapp token update.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
-var DappToken = artifacts.require("./DappToken.sol");
-var DappTokenSale = artifacts.require("./DappTokenSale.sol");
+const DappToken = artifacts.require("./DappToken.sol");
+const DappTokenSale = artifacts.require("./DappTokenSale.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(DappToken, 1000000).then(function() {
-    // Token price is 0.001 Ether
-    var tokenPrice = 1000000000000000;
-    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
-  });
+module.exports = function (deployer) {
+    deployer.deploy(DappToken, 1000000).then(function () {
+        // Token price is 0.001 Ether
+        const tokenPrice = 1000000000000000;
+        return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+    });
 };
--- truffle.js
+++ truffle.js
@@ -1,17 +1,34 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  networks: {
-    ganache: {
-      host: "127.0.0.1",
-      port: "7545",
-      network_id: "*", // match any network id
-    },
-    rinkeby: {
-      host: "localhost",
-      port: 8545,
-      network_id: 4,
-      gas: 4700000
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        ganache: {
+            host: "127.0.0.1",
+            port: "7545",
+            network_id: "*", // match any network id
+        },
+        rinkeby: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*"
+        },
+        local_dev: {
+            host: "localhost",
+            port: 8545,
+            gas: 890000,
+            network_id: "*",
+        },
+        ropsten_local: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*",
+            gas: 2900000,
+            from: "0x0c40251d0028ee2fB612911A4B843b6995b8169f"
+        },
+        ropsten: {
+            host: "https://api.infura.io/v1/jsonrpc/ropsten",
+            port: 8545,
+            network_id: "3"
+        }
     }
-  }
 };
