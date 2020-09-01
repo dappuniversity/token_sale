2ac038f7e9295818c224655fd48deec61ccde65f
Nate Bowen <nate@natebowen.com>
2020-08-31 21:42:38 -0700

    Adding explicit solc 0.4.17 in the compilers section of both truffle.js and truffle-config.js.

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,22 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: "7545",
+      network_id: "*" // match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4700000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.17"
+    }
+  }
 };
